4ab80c7518233e371170165fba60d0768db5c327
moori <daniel.moori@gmail.com>
2019-01-25 23:36:52 -0200

    basic move

---
+++ CannibalCrabs/Assets/_Game/Scripts/Player/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+    
+    
+    private void FixedUpdate()
+    {
+        var h = Input.GetAxisRaw("Horizontal");
+        var v = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(h, v).normalized * speed;
+        //if (h != 0 && v != 0) {
+        //    //rb.MovePosition(new Vector2(h,v).normalized * speed);
+        //}
+    }
+}
