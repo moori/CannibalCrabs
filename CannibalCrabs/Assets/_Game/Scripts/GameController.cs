49cf408652014770a03ef0dcfc9affa86d6c0eb3
Adriano Sudário <sudario.adriano@gmail.com>
2019-01-26 05:12:00 -0200

    festa das shell

--- CannibalCrabs/Assets/_Game/Scripts/GameController.cs
+++ CannibalCrabs/Assets/_Game/Scripts/GameController.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public Meat meatPrefab;
+    public List<Shell> shellPrefabs;
     public Player playerPrefab;
+    public int maxShells = 6;
+    public int shellsSpawned;
 
     private List<Player> players = new List<Player>();
 
-
     private IEnumerator Start()
     {
-        //StartCoroutine(SpawnMeat());
+        Debug.Log("hm " + shellPrefabs.Count);
+        for (int i = 0; i < maxShells; i++)
+            SpawnShell();
 
+        StartCoroutine(SpawnShells());
 
         List<Vector2> pos = new List<Vector2>() { new Vector2(-4, 4), new Vector2(4, 4), new Vector2(-4, -4), new Vector2(4, -4), };
         yield return new WaitForSeconds(0.5f);
@@ -26,12 +30,18 @@ public class GameController : MonoBehaviour
         }
     }
 
-    IEnumerator SpawnMeat()
+    public void SpawnShell()
     {
-        while (true)
+        Shell shell = Instantiate(shellPrefabs.GetRandom());
+        shell.transform.position = Random.insideUnitCircle * Random.Range(.5f, 6);
+        shellsSpawned++;
+    }
+
+    IEnumerator SpawnShells()
+    {
+        while (shellsSpawned < maxShells)
         {
-            var meat = Instantiate(meatPrefab);
-            meat.transform.position = Random.insideUnitCircle * Random.Range(.5f, 6);
+            SpawnShell();
             yield return new WaitForSeconds(2);
         }
     }
