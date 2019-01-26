fbf1cefda27c1f0eccdd002115b91b5327822a13
Adriano Sudário <sudario.adriano@gmail.com>
2019-01-26 18:51:27 -0200

    Bug do y + desenrolecimento de código

--- CannibalCrabs/Assets/_Game/Scripts/Player/PlayerInput.cs
+++ CannibalCrabs/Assets/_Game/Scripts/Player/PlayerInput.cs
@@ -13,24 +13,25 @@ public class PlayerInput : MonoBehaviour
 
     private void FixedUpdate()
     {
-        var h = Input.GetAxisRaw(playerString + "Horizontal");
-        var v = Input.GetAxisRaw(playerString + "Vertical");
+        float h = Input.GetAxisRaw(playerString + "Horizontal");
+        float v = Input.GetAxisRaw(playerString + "Vertical");
 
-        var aim_h = Input.GetAxisRaw(playerString + "RightHorizontal");
-        var aim_v = Input.GetAxisRaw(playerString + "RightVertical");
-
-        var shoot = Input.GetAxis(playerString + "Shoot") > 0f || Input.GetButtonDown(playerString + "Shoot");
-        var sacrifice = Input.GetButtonDown(playerString + "Sacrifice");
+        float aim_h = Input.GetAxisRaw(playerString + "RightHorizontal");
+        float aim_v = Input.GetAxisRaw(playerString + "RightVertical");
 
         player.Move(h, v);
         player.Aim(h, v, aim_h, aim_v);
+    }
+
+    private void Update()
+    {
+        bool shoot = Input.GetAxis(playerString + "Shoot") > 0f || Input.GetButtonDown(playerString + "Shoot");
+        bool sacrifice = Input.GetButtonDown(playerString + "Sacrifice");
+        
         if (shoot)
-        {
             player.Shoot();
-        }
+
         if (sacrifice)
-        {
             player.Sacrifice();
-        }
     }
 }
