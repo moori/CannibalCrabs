d2fd936b9f030fa23eb9c2246f6cce665ccba50c
Adriano Sudário <sudario.adriano@gmail.com>
2019-01-26 22:55:36 -0200

    mistério da parede

--- CannibalCrabs/Assets/_Game/Scripts/Sacrifices/Dash.cs
+++ CannibalCrabs/Assets/_Game/Scripts/Sacrifices/Dash.cs
@@ -3,19 +3,19 @@
 public class Dash : MonoBehaviour
 {
     public float damage = 10;
-    public float speed = 30;
+    public float speed = 5;
 
     private Player dasher;
 
     public void Activate(Player dasher, Vector2 initialPosition, Vector2 direction)
     {
-        dasher.Disable();
+        dasher.gameObject.SetActive(false);
         this.dasher = dasher;
         transform.position = initialPosition;
         Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
         SpriteRenderer sprite = GetComponentInChildren<SpriteRenderer>();
         rigidBody.velocity = direction * speed;
-        sprite.transform.right = direction;
+        sprite.transform.up = direction;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -23,9 +23,16 @@ public class Dash : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             Player player = collision.gameObject.GetComponent<Player>();
+
+            if (player == dasher)
+                return;
+
             player.TakeDamage(damage);
         }
 
-        dasher.Enable(transform.position);
+        dasher.gameObject.SetActive(true);
+        dasher.transform.position = transform.position;
+        Destroy(gameObject);
+        Debug.Log("giff me parede", collision.gameObject);
     }
 }
