eba8e496306ab3be6f90ca5823ec07a2a9ced6d4
moori <daniel.moori@gmail.com>
2019-01-27 17:39:05 -0200

    no message

--- CannibalCrabs/Assets/_Game/Scripts/GameController.cs
+++ CannibalCrabs/Assets/_Game/Scripts/GameController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -171,6 +172,14 @@ public class GameController : MonoBehaviour
 
         winnerText.gameObject.SetActive(true);
         winnerText.color = winner.color;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Title");
+        }
     }
 }
 
